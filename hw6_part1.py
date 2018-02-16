9e42be2c6fa2b3a8d5366053d47a66a0db087fe8
William Kim <wichki@umich.edu>
2018-02-15 20:19:56 -0500

    Pushing

--- hw6_part1.py
+++ hw6_part1.py
@@ -7,3 +7,15 @@ print('\n*********** PART 1 ***********')
 print("Mark's page -- Alt tags\n")
 
 ### Your Part 1 solution goes here
+
+#we want to read it on here
+html = requests.get("http://newmantaylor.com/gallery.html").text
+soup = BeautifulSoup(html, "html.parser")
+print(soup.find("img"))
+print(soup.find_all("img"))
+
+for i in soup.find_all("img"):
+    if i.has_attr("alt"):
+        print(i["alt"])
+    else:
+        print("There is no alternative text provided!")
--- hw6_part2.py
+++ hw6_part2.py
@@ -8,3 +8,20 @@ print('\n*********** PART 2 ***********')
 print('Michigan Daily -- MOST READ\n')
 
 ### Your Part 2 solution goes here
+
+html = requests.get("http://michigandaily.com").text
+soup = BeautifulSoup(html, "html.parser")
+# print(html)
+# print (soup)
+
+# print(soup.find('div', attrs={"class": "panel-pane pane-mostread"}))
+
+# for i in soup.find('div', attrs={"class": "panel-pane pane-mostread"}):
+#     print(i)
+chunk = soup.find('div', attrs={"class": "panel-pane pane-mostread"})
+# print(chunk)
+# print(chunk.find_all("li"))
+# print(chunk.find_all("a"))
+
+for i in chunk.find_all("a"):
+    print(i.text)
