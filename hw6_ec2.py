7b2d5786d4b5016142efbdc2708a1accd1df348c
William Kim <wichki@umich.edu>
2018-02-18 20:55:59 -0500

    still working on extra credit 2

--- hw6_ec2.py
+++ hw6_ec2.py
@@ -7,3 +7,16 @@ print('\n*********** EXTRA CREDIT 2 ***********')
 print('Re-sort Atheletes by State\n')
 
 ### Your Extra Credit 2 solution goes here
+
+athletic_html = requests.get("https://www.athletic.net/CrossCountry/Results/Meet.aspx?Meet=135827").text
+
+athletic_soup = BeautifulSoup(athletic_html, "html.parser")
+# print(athletic_soup)
+
+table_rows = athletic_soup.find_all(id="D_557666")
+# print(table_rows)
+
+for i in table_rows:
+    table_cells = i.find_all('td')
+    # print(i.text)
+print(table_cells)
